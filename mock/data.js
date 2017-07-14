7da7845fb8898833257a9567ec247798267ac0ef
Tak1s <d.kornienko@favbet.com>
2017-07-14 19:01:31 +0300

    write task

---
+++ mock/data.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        alias: 'Animals',
+        name: 'Животные',
+        link: '/animals',
+    },
+    {
+        alias: 'Goods',
+        name: 'Товары',
+        link: '/goods',
+    },
+    {
+        alias: 'Services',
+        name: 'Услуги',
+        link: '/services',
+    },
+    {
+        alias: 'All about pets',
+        name: 'Все о питомцах',
+        link: '/allAboutPets',
+    },
+    {
+        alias: 'Promotions',
+        name: 'Акции',
+        link: '/promotions',
+    },
+];
