abaf3e3934f0ca26bbfc41831da1b9cdec5d2098
igordejak <igordejak@betinvest.com>
2019-09-27 14:13:35 +0300

    dom elements

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 import data from './mock/data';
+const any_string = Math.random();
+
+//1
+document.body.innerHTML += `<p> ${any_string} </p>`;
+
+//2
+let random = document.createTextNode(any_string);
+let paragraph = document.createElement("p");
+paragraph.appendChild(random);
+document.body.appendChild(paragraph);
+
+//3
+document.body.insertAdjacentHTML( 'beforeend', `<p> ${any_string} </p>` );
